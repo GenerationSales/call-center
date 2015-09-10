a78c3f009873fae1501f84b32699cae7a981f449
zarianlewis <zarian.a.lewis@gmail.com>
2015-09-10 15:07:32 -0600

    Change to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150910035052) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.text     "name"
     t.integer  "company_id"
