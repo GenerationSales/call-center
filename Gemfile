0c65f13929d470a36fa4ccbf69850f74907aa4be
zarianlewis <zarian.a.lewis@gmail.com>
2015-09-12 12:55:19 -0600

    added basics of devise

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'simple_form'
 gem "font-awesome-rails"
+gem 'devise'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor', group: :production
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts
 
   resources :campaigns
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'companies#index'
+   root to: 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
