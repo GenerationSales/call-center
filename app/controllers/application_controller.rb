d661e3b920fc0e3b479e265a28434626c578ce69
Zarian Lewis <zarianlewis@Zarians-MacBook-Pro.local>
2015-09-20 17:05:01 -0600

    fixed routes for logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
+def after_sign_in_path_for(resource)
+  admin_companies_path
+end
+
   protected
 
   def configure_permitted_parameters
@@ -11,5 +17,13 @@ class ApplicationController < ActionController::Base
       :password_confirmation, :remember_me, :avatar, :avatar_cache) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password,
       :password_confirmation, :current_password, :avatar, :avatar_cache) }
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == "new"
+      "login"
+    else
+      "application"
+    end
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,18 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( frontend.css )
 Rails.application.config.assets.precompile += %w( frontend.js )
+Rails.application.config.assets.precompile += %w( plugins/boostrapv3/css/bootstrap-theme.min.css )
+Rails.application.config.assets.precompile += %w( css/animate.min.css )
+Rails.application.config.assets.precompile += %w( css/style.css )
+Rails.application.config.assets.precompile += %w( css/responsive.css )
+Rails.application.config.assets.precompile += %w( css/custom-icon-set.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap-sprockets.js )
+Rails.application.config.assets.precompile += %w( plugins/pace/pace.min.js )
+Rails.application.config.assets.precompile += %w( plugins/jquery-validation/js/jquery.validate.min.js )
+Rails.application.config.assets.precompile += %w( js/login.js )
+Rails.application.config.assets.precompile += %w( plugins/boostrapv3/css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( plugins/font-awesome/css/font-awesome.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
